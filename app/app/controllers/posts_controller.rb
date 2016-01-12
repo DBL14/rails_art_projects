a888a6f195b9c333a8ffea987600b77f38a96911
David Leslie <davidleslie@Davids-MacBook-Pro-3.local>
2016-01-12 21:04:25 +0000

    created follow and unfollow forms and controller for relationships

--- app/app/controllers/posts_controller.rb
+++ app/app/controllers/posts_controller.rb
@@ -12,4 +12,14 @@ class PostsController < ApplicationController
 	def new
 		@post = Post.new
 	end
+
+	def create
+		@post = Post.create(params[:id, :title, :body, :url])
+		if @post.save
+			redirect_to post_path(@post),
+					notice: "You successfully created a new post"
+		else
+			render :new, alert: "Post creation failed"
+		end
+	end
 end
