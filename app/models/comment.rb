3d0ed5394aa706dae795897c0dd219086f00e98f
David Leslie <davidleslie@Davids-MacBook-Pro-3.local>
2016-01-14 18:15:17 +0000

    commenting out validates method in unused comments model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
 
-  vaildates :user_id, presence: true
-  vaildates :post_id, presence: true
+  # vaildates :user_id, presence: true
+  # vaildates :post_id, presence: true
 end
