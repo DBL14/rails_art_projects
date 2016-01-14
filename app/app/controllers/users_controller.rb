0c11d4c938aef2c2da49bb38f3869b8a350d977f
David Leslie <davidleslie@Davids-MacBook-Pro-3.local>
2016-01-14 15:52:57 +0000

    changed foundation gem version

--- app/app/controllers/users_controller.rb
+++ app/app/controllers/users_controller.rb
@@ -19,12 +19,23 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-    if user.update_attributes(user_params)
+      render 'edit'
+  end
+
+  def update
+    @user = current_user
+    if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
-      redirect_to @user
+      redirect_to posts_path
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    @user = User.find(params[:id]).destroy
+   flash[:success] = "Profile destroyed."
+   redirect_to root_url
   end
 
   private
--- app/db/seeds.rb
+++ app/db/seeds.rb
@@ -6,7 +6,18 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-u1 = User.create!(name: 'bob', email: 'email', password: 'password1')
+u1 = User.create!(name: 'Bob', email: 'bob@email', password: 'password')
 
-u2 = User.create!(name: 'dave', email: 'dave@email.com', password: 'password2')
+u2 = User.create!(name: 'David', email: 'dave@email.com', password: 'password')
 
+u3 = User.create!(name: 'Dan', email: 'dan@email.com', password: 'password')
+
+u1.posts.create!(title: 'first post', body: 'This is my first painting', url: 'http://unhyphenatedamerica.org/wp-content/uploads/2015/02/abstract-art-mother-earth-1.jpg', user_id: 1)
+
+u1.posts.create!(title: 'painting', body: 'I experimented with different colours for this painting', url: 'http://digipraim.com/data_images/out/3/587178-picture-in-the-park-art.jpg', user_id: 1)
+
+u2.posts.create!(title: 'First painting', body: 'This is a painting I worked on during the Christmas holiday', url: 'http://4.bp.blogspot.com/-okyzluX_zlw/UR6pt0fMp9I/AAAAAAAAAPk/bbeepDoIQDo/s1600/tumblr_mhewiwW6ib1r3wk1zo2_500-1.jpg', user_id: 2)
+
+u3.posts.create!(title: 'Zebra', body: 'This is an idea I have. Should I continue with this?', url: 'http://img07.deviantart.net/d8a2/i/2010/165/b/2/zebra_of_a_different_color_by_art_siren717.jpg', user_id: 3)
+
+u3.posts.create!(title: 'New Painting', body: 'This is another painting I am working on.', url: 'https://katiebreitung.files.wordpress.com/2012/11/flat550x550075f.jpg', user_id: 3)
